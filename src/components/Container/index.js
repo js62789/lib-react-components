ffcf83f96e527c49673b1699bebf882bcc46225d
Jeffrey Smith <js62789@gmail.com>
2017-06-11 09:54:44 -0400

    Add fluid container

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -4,11 +4,17 @@ import styles from './styles.css';
 export class Container extends React.Component {
   render() {
     const Element = this.props.as || 'div';
-    const { className, ...other } = this.props;
-    const classNames = [ styles.container ];
+    const { className, fluid, ...other } = this.props;
+    const classNames = [];
 
     if (className) {
       classNames.push(className);
+    }
+
+    if (fluid) {
+      classNames.push(styles.containerFluid);
+    } else {
+      classNames.push(styles.container);
     }
 
     return (
